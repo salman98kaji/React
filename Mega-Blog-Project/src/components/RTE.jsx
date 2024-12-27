1c56579272f5c21152b319485fba35344ab04fee
salman98kaji <salman2017kaji@gmail.com>
2024-12-27 23:45:48 +0530

    few comments added

--- Mega-Blog-Project/src/components/RTE.jsx
+++ Mega-Blog-Project/src/components/RTE.jsx
@@ -1,5 +1,6 @@
+
 import React from 'react'
-import { Editor } from '@tinymce/tinymce-react'
+import { Editor } from '@tinymce/tinymce-react' 
 import {Controller} from 'react-hook-form'
 
 export default function RTE({name, control, defaultValue=''}) {
@@ -40,9 +41,8 @@ export default function RTE({name, control, defaultValue=''}) {
                     "wordcount",
                     "anchor",
                 ],
-                toolbar:
-                    "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-                    content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                toolbar:"undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
             }}
             onEditorChange={onChange}
             />
@@ -52,3 +52,19 @@ export default function RTE({name, control, defaultValue=''}) {
   )
 }
 
+//The RTE component is a reusable component that we can use to add a rich text editor to our forms.
+//The component takes several props, including name, control, and defaultValue.
+//The name prop is used to set the name of the editor field, which is used to identify the field in the form.
+//The control prop is used to integrate the editor with the react-hook-form library, which is used to manage form state.
+//The defaultValue prop is used to set the initial value of the editor field.
+//label is a prop that is used to set the label for the editor field, which is displayed above the editor.
+//Controller is a component from react-hook-form that allows us to integrate the TinyMCE editor with our form, so that changes to the editor are reflected in the form state.
+//The render prop of the Controller component is used to render the Editor component and pass the onChange function to the onEditorChange prop of the Editor component.
+//field is a prop that is passed to the render function of the Controller component, which contains the onChange function that is used to update the form state when the editor content changes.
+//Editor is a component from the TinyMCE library that is used to render the rich text editor, which allows users to format text, add images, and more.
+//The initialValue prop of the Editor component is used to set the initial value of the editor field, which is passed from the defaultValue prop of the RTE component.
+//The init prop of the Editor component is used to configure the editor, including setting the height, plugins, toolbar, and content style.
+//plugins means the features that are available in the editor, such as image, lists, link, and more.
+//toolbar is a set of buttons that are displayed above the editor, which allow users to format text, add images, and more, such as undo, redo, bold, italic, and more.
+//content_style is used to set the font family and font size of the editor content.
+//onEditorChange is a prop of the Editor component that is called when the editor content changes, and it updates the form state using the onChange function passed from the Controller component.
