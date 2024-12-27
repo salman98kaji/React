4e94849754f3f63ccfaf60da9194b61258e0c7aa
salman98kaji <salman2017kaji@gmail.com>
2024-12-27 23:11:11 +0530

    RTE, PostForm

--- Mega-Blog-Project/src/components/index.js
+++ Mega-Blog-Project/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./footer/footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
 
-export {Header, Footer, Logo, Container, LogoutBtn}
+export {Header, Footer, Logo, Container, LogoutBtn, RTE}
